27b786183565debfb3be40ff8f7fbc616a23e4a3
Lakshya-Saini <saini.lakshya1998@gmail.com>
2020-07-22 14:26:55 +0530

    Modified js files

--- dist/assets/js/firebase.js
+++ dist/assets/js/firebase.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyDuCtbIS3xZnZvCthHZRAXs35WU6vprLrQ",
-  authDomain: "js-ecommerce-test-app.firebaseapp.com",
-  databaseURL: "https://js-ecommerce-test-app.firebaseio.com",
-  projectId: "js-ecommerce-test-app",
-  storageBucket: "js-ecommerce-test-app.appspot.com",
-  messagingSenderId: "237515494747",
-  appId: "1:237515494747:web:f52a2ae4d5fb90223e367e",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 firebase.initializeApp(firebaseConfig);
